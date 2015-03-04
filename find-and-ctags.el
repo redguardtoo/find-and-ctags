8e43e64a887cf0093118b16e6340824f4e788448
Chen Bin <chenbin.sh@gmail.com>
2015-03-04 18:53:28 +1100

    almost done

--- find-and-ctags.el
+++ find-and-ctags.el
@@ -20,27 +20,80 @@
 (defvar fac-gnu-find-executable nil
   "The path of GNU Find. If it's nil, it will be automatically detected.")
 
+(defvar fac-ctags-executable nil
+  "The path of Ctags. If it's nil, it will be automatically detected.")
+
+(defun fac--guess-gnu-find ()
+  (let (rlt)
+    (if (eq system-type 'windows-nt)
+        (cond
+         ((executable-find "c:\\\\cygwin64\\\\bin\\\\find")
+          (setq rlt "c:\\\\cygwin64\\\\bin\\\\find"))
+         ((executable-find "d:\\\\cygwin64\\\\bin\\\\find")
+          (setq rlt "d:\\\\cygwin64\\\\bin\\\\find"))
+         ((executable-find "e:\\\\cygwin64\\\\bin\\\\find")
+          (setq rlt "e:\\\\cygwin64\\\\bin\\\\find"))
+         ((executable-find "c:\\\\cygwin\\\\bin\\\\find")
+          (setq rlt "c:\\\\cygwin\\\\bin\\\\find"))
+         ((executable-find "d:\\\\cygwin\\\\bin\\\\find")
+          (setq rlt "d:\\\\cygwin\\\\bin\\\\find"))
+         ((executable-find "e:\\\\cygwin\\\\bin\\\\find")
+          (setq rlt "e:\\\\cygwin\\\\bin\\\\find"))
+         (t (setq rlt "find"))))
+    rlt))
+
+(defun fac--guess-ctags ()
+  (let (rlt "ctags")
+    (if (eq system-type 'windows-nt)
+        (cond
+         ((executable-find "c:\\\\cygwin64\\\\bin\\\\ctags")
+          (setq rlt "c:\\\\cygwin64\\\\bin\\\\ctags"))
+         ((executable-find "d:\\\\cygwin64\\\\bin\\\\ctags")
+          (setq rlt "d:\\\\cygwin64\\\\bin\\\\ctags"))
+         ((executable-find "e:\\\\cygwin64\\\\bin\\\\ctags")
+          (setq rlt "e:\\\\cygwin64\\\\bin\\\\ctags"))
+         ((executable-find "c:\\\\cygwin\\\\bin\\\\ctags")
+          (setq rlt "c:\\\\cygwin\\\\bin\\\\ctags"))
+         ((executable-find "d:\\\\cygwin\\\\bin\\\\ctags")
+          (setq rlt "d:\\\\cygwin\\\\bin\\\\ctags"))
+         ((executable-find "e:\\\\cygwin\\\\bin\\\\ctags")
+          (setq rlt "e:\\\\cygwin\\\\bin\\\\ctags"))
+         (t (setq rlt "ctags"))))
+    rlt))
+
 ;;;###autoload
-(defun fac-create-tags-if-needed (SRC-DIR FIND-OPTS CTAGS-OPTS &optional FORCE)
-  "Create TAGS if need and return the full path it"
+(defun fac-run-ctags-if-needed (SRC-DIR FIND-OPTS CTAGS-OPTS &optional FORCE)
+  "Ask ctags to create TAGS and return the full path of TAGS"
   ;; TODO save the CTAGS-OPTS into hash
   (let ((dir (file-name-as-directory (file-truename SRC-DIR)) )
-        (gnufind (if *win32* "c:\\\\cygwin64\\\\bin\\\\find" "find"))
+        (find-exe (if fac-gnu-find-exe fac-gnu-find-exe (fac--guess-gnu-find)))
+        (ctags-exe (if fac-ctags-exe fac-find-exe (fac--guess-ctags)))
         old-dir
         file
         cmd)
     (setq file (concat dir "TAGS"))
-    (if (string= "" FIND-OPTS) (setq FIND-OPTS "-not -size +24k"))
     (when (or FORCE (not (file-exists-p file)))
       (setq old-dir default-directory)
-      ;; "cd dir && find . -name blah | ctags" will NOT work on windows
+      ;; "cd dir && find . -name blah | ctags" will NOT work on windows cmd window
       (setq default-directory dir)
-      ;; use relative directory because TAGS is shared between cygwin and win32 Emacs
-      (setq cmd (format "%s . -type f -not -name 'TAGS' %s | ctags -e %s -L -" gnufind FIND-OPTS CTAGS-OPTS))
+      ;; use relative directory because TAGS is shared between Cygwin and Window
+      (setq cmd (format "%s . -type f -not -name 'TAGS' %s | %s -e %s -L - &"
+                        find-exe
+                        FIND-OPTS
+                        ctags-exe
+                        CTAGS-OPTS))
       (shell-command cmd)
       ;; restore default directory
       (setq default-directory old-dir))
     file))
+
+;;;###autoload
+(defun fac-current-path-match-pattern-p (REGEX)
+  "Is current directory match the REGEX?"
+  (let ((dir (if (buffer-file-name)
+                 (file-name-directory (buffer-file-name))
+               "")))
+    (string-match-p REGEX dir)))
 
 (provide 'find-and-ctags)
 
