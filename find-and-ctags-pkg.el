d8f1ace14fc45a3ba9942618ae4687d530025142
Chen Bin <chenbin.sh@gmail.com>
2015-09-22 20:11:48 +1000

    follow melpa code standard v0.0.4

--- find-and-ctags-pkg.el
+++ find-and-ctags-pkg.el
@@ -1,2 +1,2 @@
-(define-package "find-and-ctags" "0.0.3"
+(define-package "find-and-ctags" "0.0.4"
                 "Create and update TAGS by combining Find and Ctags for any language on Winows/Linux/OSX")
